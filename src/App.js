3f487a42e52b64e2d84c5a47a5da731a1ac4e406
roshanpaudel <paudelroshan93@gmail.com>
2025-05-04 23:44:31 +1000

    basic UI element created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div>
-    hello world
-  </div>;
+  return (
+    <div>
+      <form className="card">
+        <label>inputtd value</label>
+        <input type="text" placeholder="enter name"></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
