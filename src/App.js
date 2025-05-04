4d1784d1a1ac9959dd1fc91e763b18cfbc39b329
roshanpaudel <paudelroshan93@gmail.com>
2025-05-04 23:58:21 +1000

    form element updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <form className="card">
+      <form className="card d-flex gap-5 col-8 my-4 mx-auto p-4 bg-info rounded-4">
         <label>inputtd value</label>
         <input type="text" placeholder="enter name"></input>
-        <button>Submit</button>
+        <button className="bg-primary text-white">Submit</button>
       </form>
     </div>
   );
